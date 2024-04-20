2f7023b0a7d9a4888d889723ad6964107536cfdf
k-akzw <k-akazawa@onu.edu>
2024-04-20 04:37:51 -0400

    created database on macbook pro

--- Quiz/QuizModel.swift
+++ Quiz/QuizModel.swift
@@ -42,18 +42,4 @@ class QuizModel {
 		}
 		return li.shuffled()
 	}
-
-  // returns list of questions from database
-//  func getQuestions(_ questions: FetchedResults<MCQuestions>, qType: QuestionType) -> [Question] {
-//    var li: [Question] = []
-//    // iterate through all questions in the database
-//    // and add all questions which has the same question type to @li
-//    for q in questions {
-//      if (q.qType == qType.rawValue) {
-//        li.append(Question(id: q.id!, question: q.question!, choice1: q.choice1 ?? "", choice2: q.choice2 ?? "", choice3: q.choice3 ?? "", choice4: q.choice4 ?? "", answer: Int(q.answer), qType: QuestionType(rawValue: q.qType!) ?? QuestionType.mc, res: q))
-//      }
-//    }
-//    // shuffle the order of questions
-//    return li.shuffled()
-//  }
 }
